1cc6646f8077e049b090e1036549b78601558610
shivammutreja <shivammutreja25@gmail.com>
2016-09-20 13:56:06 +0530

    implemented basic user registration

--- check_credentials.py
+++ check_credentials.py
@@ -6,20 +6,24 @@ import hashlib
 conn = pymongo.MongoClient()
 db = conn.test_db
 coll = db.test_coll
+types = {'hod': True, 'user': False}
 
 class CheckCredentials:
 	@staticmethod
 	def check(username, password):
-		user = coll.find_one({'user': username, 'pwd': password}, {'_id': False})
+		print password
+		pass_hash = hashlib.md5(password).hexdigest()
+		user = coll.find_one({'user': username, 'pwd': pass_hash}, {'_id': False})
 		return user
 
 	@staticmethod
-	def save(username, password, name, age):
+	def save(username, password, name, age, user_type):
 		user_hash = hashlib.md5(username).hexdigest()
 		if coll.find_one({'user_id': user_hash}):
 			return True
 		else:
-			user = coll.insert({'user': username, 'pwd': password, 'name':\
-		 	name, 'age': age, 'user_id': user_hash})
+			pass_hash = hashlib.md5(password).hexdigest()
+			user = coll.insert({'user': username, 'pwd': pass_hash, 'name':\
+		 	name, 'age': age, 'user_id': user_hash, 'hod': types.get(user_type, '')})
 
 
